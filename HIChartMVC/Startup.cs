fc636668c085709bc678c53e2d891e602275f353
IniyenMA <iniyenarjunan@ymail.com>
2015-09-29 12:16:20 -0700

    Add All Files

---
+++ HIChartMVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(HIChartMVC.Startup))]
+namespace HIChartMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
